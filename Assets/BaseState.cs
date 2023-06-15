2bab61b5a0c6dc776ee32b897e9865ecec52b922
Samwel Suleiman <schmittsluke@gmail.com>
2023-06-15 14:52:25 +0200

    created BaseState.cs

---
+++ Assets/BaseState.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseState : MonoBehaviour
+{
+    public virtual void EnterState()
+    {
+
+    }
+
+    public virtual void UpdateState()
+    {
+
+    }
+
+    public virtual void LeaveState()
+    {
+
+    }
+
+}
