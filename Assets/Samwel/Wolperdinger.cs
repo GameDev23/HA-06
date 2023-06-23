8b6a714c09d1a55a50e87ef8b596872af2f22c81
Samwel Suleiman <94542984+ABlessingFromTheLord@users.noreply.github.com>
2023-06-23 17:40:23 +0200

    Changed colors of rooms

--- Assets/Samwel/Wolperdinger.cs
+++ Assets/Samwel/Wolperdinger.cs
@@ -13,11 +13,14 @@ public class Wolperdinger : Enemy
     public override void Attack()
     {
         animator.SetTrigger("triggerAttack");
+        StartCoroutine(AttackRoutine());
     }
 
     public override void Charge()
     {
         animator.SetTrigger("triggerCharge");
+        StartCoroutine(ChargeRoutine());
+
     }
 
     public override void ChargeAnimation()
@@ -50,6 +53,48 @@ public class Wolperdinger : Enemy
     void Update()
     {
         
+    }
+
+    IEnumerator ChargeRoutine()
+    {
+
+        while (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+
+            yield return null;
+        }
+
+        while (animator.GetCurrentAnimatorStateInfo(0).IsName("Charge"))
+        {
+            yield return null;
+        }
+
+        //End of charging  now do dmg
+        if (!Manager.Instance.isPlayerDefending)
+            Manager.Instance.PlayerHp -= Manager.Instance.PlayerHp;
+        Manager.Instance.isPlayerDefending = false;
+    }
+
+    IEnumerator AttackRoutine()
+    {
+
+        while (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+
+            yield return null;
+        }
+
+        while (animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        {
+            yield return null;
+        }
+
+
+        //End of charging  now do dmg
+        int rand = Random.Range(1, 4);
+        if (!Manager.Instance.isPlayerDefending)
+            Manager.Instance.PlayerHp = Manager.Instance.PlayerHp - rand < 0 ? 0 : Manager.Instance.PlayerHp - rand;
+        Manager.Instance.isPlayerDefending = false;
     }
 
 
